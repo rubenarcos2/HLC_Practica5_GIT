4abfc84059f9bbe5e98945b9413516ebe887c8d5
rubenarcos2 <rubenarcos2@gmail.com>
2021-01-07 20:09:09 +0100

    feat(app.js)

---
+++ app.js
@@ -0,0 +1,3 @@
+console.log("Inicio de la aplicación");
+console.log("Programador 1 - Tarea 1");
+console.log("Esta línea corresponde al trabajo de la tarea 1, realizada y asignada en la rama 'feature1arcosortega'");
