a58a4408ccb74294519110c44a71f30bf9caadf5
rubenarcos2 <rubenarcos2@gmail.com>
2021-01-08 16:42:51 +0100

    feat(feat2, commit 2º)

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ console.log("Programador 1 - Tarea 1");
 console.log("Esta línea corresponde al trabajo de la tarea 1, realizada y asignada en la rama 'feature1arcosortega'");
 console.log("Programador 1 - Tarea 2");
 console.log("Esta línea corresponde al trabajo de la tarea 2, realizada y asignada en la rama 'feature2arcosortega'");
+console.log("Añado modificación por tarea 2");
