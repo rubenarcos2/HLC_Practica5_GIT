f06faeabfcac2fe5601271822a461886290f76ed
rubenarcos2 <rubenarcos2@gmail.com>
2021-01-08 16:18:43 +0100

    feat(feat2)

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log("Inicio de la aplicación");
 console.log("Programador 1 - Tarea 1");
 console.log("Esta línea corresponde al trabajo de la tarea 1, realizada y asignada en la rama 'feature1arcosortega'");
+console.log("Programador 1 - Tarea 2");
+console.log("Esta línea corresponde al trabajo de la tarea 2, realizada y asignada en la rama 'feature2arcosortega'");
